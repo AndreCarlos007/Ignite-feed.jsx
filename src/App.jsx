4f97df9c3b3670085e1d9ea09424f98dca6290c0
AndreCarlos007 <andreoliveiracx007@gmail.com>
2025-02-02 13:18:21 -0300

    :sparkles: feat: Projeto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ const posts = [
     {
       id: 1,
       author: {
-        avatarUrl: 'https://github.com/diego3g.png',
-        name: 'Diego Fernandes',
-        role: 'CTO @ Rocketseat'
+        avatarUrl: 'https://github.com/guilhermetrutaa.png',
+        name: 'Guilherme Truta',
+        role: 'Software Developer'
       },
       content: [
         { type: 'paragraph', content: 'Fala galeraa 👋'},
@@ -22,21 +22,7 @@ const posts = [
       ],
       publishedAt: new Date('2025-02-02T20:00:00Z')
     },
-    {
-      id: 2,
-      author: {
-        avatarUrl: 'https://github.com/maykbrito.png',
-        name: 'Mayk Brito',
-        role: 'Educator @ Rocketseat'
-      },
-      content: [
-        { type: 'paragraph', content: 'Fala galeraa 👋'},
-        { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
-        { type: 'link', content: 'jane.design/doctorcare'} 
-  
-      ],
-      publishedAt: new Date('2025-02-02T20:00:00Z')
-    },
+    
 ];
 
 
